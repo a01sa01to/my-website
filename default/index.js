66b57f6f83d541def7e65401777bfc335a350c22
Asa <26807394+a01sa01to@users.noreply.github.com>
2020-10-01 22:07:26 +0900

    Update: srv追加

--- default/index.js
+++ default/index.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const port = process.env.PORT || 3000;
 const index = '/index.html';
-const _403 = '/err/403.html'
-const _404 = '/err/404.html'
-const _500 = '/err/500.html'
+const _403 = '/srv/err/403.html'
+const _404 = '/srv/err/404.html'
+const _500 = '/srv/err/500.html'
 
 const app = express();
 
 app.use((req,res)=>{
 	req.url = "/srv" + req.url
-	res.header("Access-Control-Allow-Origin","*")
 	res.sendFile(req.url);
 })
 app.use((req,res)=>{
