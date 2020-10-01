8596efab207aaefad86d33ce05f56ec59b1d4adc
a01sa01to <a01sa01to@users.noreply.github.com>
2020-10-01 13:19:33 +0000

    deploy: c2aff24dd234b00ab0b51a56a11d03b6d5ca34ff

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 const express = require('express');
 const port = process.env.PORT || 3000;
-const index = '/index.html';
-const _403 = '/srv/err/403.html'
-const _404 = '/srv/err/404.html'
-const _500 = '/srv/err/500.html'
+const _403 = '/err/403.html'
+const _404 = '/err/404.html'
+const _500 = '/err/500.html'
 
 const app = express();
 
-app.use((req,res)=>{
-	req.url = "/srv" + req.url
-	res.sendFile(req.url);
-})
-app.use((req,res)=>{
-	req.url = `/srv${req.url}.html`
-	res.sendFile(req.url);
-})
+app.use('/',express.static('/'));
 app.use((req,res)=>{
 	res.status(403).sendFile(_403);
 })
