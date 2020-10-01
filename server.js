08d3e63957d610ae723d026ea12d94d8efae51cc
a01sa01to <a01sa01to@users.noreply.github.com>
2020-10-01 14:50:00 +0000

    deploy: e1c6833d66ac3fc14415840a7150e7083de76901

--- server.js
+++ server.js
@@ -1,23 +1,17 @@
 const express = require('express');
+const boom = require('@hapi/boom');
 const port = process.env.PORT || 3000;
 const path = require('path')
-const _403 = '403.html'
-const _404 = '404.html'
-const _500 = '500.html'
 
 const app = express();
 
 app.use((req,res)=>{
 	res.sendFile(path.join(__dirname,req.url));
 })
-app.use((req,res)=>{
-	res.status(403).sendFile(path.join(__dirname,'err',_403));
-})
-app.use((req,res)=>{
-	res.status(404).sendFile(path.join(__dirname,'err',_404));
-})
-app.use((req,res)=>{
-	res.status(500).sendFile(path.join(__dirname,'err',_500));
+app.use((err,req,res,next)=>{
+	console.error(err);
+	// res.status(err.statusCode).json(err);
+	res.status(err.statusCode).sendFile(path.join(__dirname,`err/${err.statusCode}.html`));
 })
 
 app.listen(port, ()=>console.log(`Listening on ${port}`));
