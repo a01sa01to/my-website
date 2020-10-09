6afcb86caebc502255c1f0923d9d9dd3a5b36a4f
a01sa01to <a01sa01to@users.noreply.github.com>
2020-10-09 13:12:10 +0000

    deploy: dcc8ff25ac0785106544213fd155897367701411

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const fs = require('fs');
 const port = process.env.PORT || 3000;
 const path = require('path')
 
 const app = express();
 
 app.use((req,res)=>{
+	if(!req.path.includes(".")){
+		const newPath = path.join(__dirname,`${req.path}.html`);
+		if(fs.existsSync(path)){
+			res.sendFile(newPath);
+			return;
+		}
+	}
 	res.sendFile(path.join(__dirname,req.url));
 })
+
 app.use((err,req,res,next)=>{
 	console.error(err);
 	res.status(err.statusCode).sendFile(path.join(__dirname,`err/${err.statusCode}.html`));
