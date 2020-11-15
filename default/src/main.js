75c4b5280e776d30db8d9e3a7dc2d0a71a78ec12
Asa <26807394+a01sa01to@users.noreply.github.com>
2020-11-15 16:10:05 +0900

    Add share button

--- default/src/main.js
+++ default/src/main.js
@@ -15,4 +15,13 @@ window.addEventListener('DOMContentLoaded',()=>{
   });
 
   document.querySelectorAll('img').forEach(_=>_.oncontextmenu=()=>false);
+
+	window.addEventListener("load",()=>{
+		$("a.sBtn").each(function(){
+			let a = $(this).attr("href");
+			a = a.replace(/PAGEURL/g, encodeURIComponent(location.href));
+			a = a.replace(/PAGETITLE/g, $(".post-title").text());
+			$(this).attr("href", a)
+		})
+	})
 })
