c53bb832a32572e13512575ae6c9e100ee62c9a0
Asa <26807394+a01sa01to@users.noreply.github.com>
2020-11-16 00:09:39 +0900

    Create main func

---
+++ blog/src/main.js
@@ -0,0 +1,39 @@
+const $ = require('jquery');
+const ace = require('ace-builds/src-noconflict/ace.js');
+
+window.addEventListener('DOMContentLoaded',()=>{
+  document.querySelectorAll('img').forEach(_=>_.oncontextmenu=()=>false);
+
+	$("footer a.sBtn").each(function(){
+		let a = $(this).attr("href");
+		a = a.replace(/PAGEURL/g, encodeURIComponent(location.href));
+		a = a.replace(/PAGETITLE/g, $(".post-title").text());
+		$(this).attr("href", a)
+	})
+
+	const codeHighlight = document.querySelectorAll('div.highlighter-rouge');
+	let editorlist = [];
+	codeHighlight.forEach(_=>{
+		const lang = _.classList.value.split(' ').filter(__=>__.includes('language'))[0].replace('language-','');
+		_.setAttribute('data-lang',lang)
+		_.className = ""
+	})
+	ace.config.set('basePath','/ace-mode/')
+	for (let i = 0; i < codeHighlight.length; i++) {
+		const e = codeHighlight[i];
+		e.setAttribute('id',`editor_${i}`);
+		const code = e.innerText;
+		e.innerHTML = "";
+		const editor = ace.edit(`editor_${i}`,{
+			mode: `ace/mode/${e.getAttribute('data-lang')}`,
+			tabSize: 2,
+			useSoftTabs: true,
+			wrap: true,
+			readOnly: true,
+		})
+		editor.setTheme('ace/theme/dracula')
+		editor.setValue(code);
+		editor.gotoLine(1);
+		editorlist.push(editor);
+	}
+})
