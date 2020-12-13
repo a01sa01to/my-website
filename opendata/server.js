e4e4fb9cd0139dad663ab2a8e67c635987733d5f
Asa <26807394+a01sa01to@users.noreply.github.com>
2020-12-13 11:04:20 +0900

    No accessable file config

--- opendata/server.js
+++ opendata/server.js
@@ -5,8 +5,28 @@ const path = require('path')
 // const axios = require('axios');
 const app = express();
 
+const notAllowed = [
+	"node_modules/",
+	"src/",
+	".googleconfig",
+	".nojekyll",
+	"app.yaml",
+	"bundle.js.LICENSE.txt",
+	"cloudbuild.yaml",
+	"package-lock.json",
+	"package.json",
+	"server.js",
+	"source-context.json",
+	"webpack.config.js"
+]
+
 app.use((req,res)=>{
 	console.log(req.url);
+	if(notAllowed.includes(req.path)){
+		res.status(404).sendFile(path.join(__dirname,`err/404.html`));
+		return;
+	}
+
 	if(!req.path.includes(".")){
 		const rewritePath = path.join(__dirname,`${req.url}.html`);
 		if(fs.existsSync(rewritePath)){
