f714a0f63fd68624b2512fbb76be13d4a8a9627f
a01sa01to <a01sa01to@users.noreply.github.com>
2021-02-14 14:57:51 +0000

    deploy: 93440aa43234260ee70e36c69e21830e70c3a321

--- src/main.js
+++ src/main.js
@@ -26,7 +26,7 @@ window.addEventListener('DOMContentLoaded',()=>{
   const query = location.search.substring(1).split("&");
 	const lang_query = query.filter(_=>_.startsWith("lang"))[0];
 	const lang_cookie = document.cookie.split('; ').filter(_=>_.startsWith('lang'))[0];
-	const lang = lang_cookie || lang_query;
+	const lang = lang_query || lang_cookie;
   if(!lang || lang.split('=')[1] === "ja"){
     $('html').attr('lang','ja');
     $('[lang=en]').remove();
