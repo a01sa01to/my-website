ea3a7ce15b9341112fa86a586df3d3e2dd5b2396
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-02-14 22:34:30 +0900

    Update: defaultの英語対応

--- default/src/main.js
+++ default/src/main.js
@@ -22,4 +22,19 @@ window.addEventListener('DOMContentLoaded',()=>{
     a = a.replace(/PAGETITLE/g, $("title").text());
     $(this).attr("href", a)
   })
+
+  const query = location.search.substring(1).split("&");
+	const lang_query = query.filter(_=>_.startsWith("lang"))[0];
+	const lang_cookie = document.cookie.split('; ').filter(_=>_.startsWith('lang'))[0];
+	const lang = lang_cookie || lang_query;
+  if(!lang || lang.split('=')[1] === "ja"){
+    $('html').attr('lang','ja');
+    $('[lang=en]').remove();
+		document.cookie = 'lang=ja;domain=a01sa01to.com;max-age=31536000;secure'
+  }
+  else{
+    $('html').attr('lang','en');
+    $('[lang=ja]').remove();
+		document.cookie = 'lang=en;domain=a01sa01to.com;max-age=31536000;secure'
+  }
 })
