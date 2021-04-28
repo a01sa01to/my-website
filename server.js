a0d8a08f6d140d4cb3921660ebd004cb42214989
a01sa01to <a01sa01to@users.noreply.github.com>
2021-04-28 18:32:35 +0900

    deploy: 24067deeaaf62c8b22c44703aa7580f3f91cec00

--- server.js
+++ server.js
@@ -1,45 +1,11 @@
-const express = require('express');
-const fs = require('fs');
-const port = process.env.PORT || 3000;
+const express = require('express')
+const port = process.env.PORT || 3000
 const path = require('path')
 
-const app = express();
+const app = express()
 
-const notAllowed = [
-	"node_modules/",
-	"src/",
-	".googleconfig",
-	".nojekyll",
-	"app.yaml",
-	"bundle.js.LICENSE.txt",
-	"cloudbuild.yaml",
-	"package-lock.json",
-	"package.json",
-	"server.js",
-	"source-context.json",
-	"webpack.config.js",
-];
-
-app.use((req,res)=>{
-	console.log(req.url);
-	if(notAllowed.includes(req.path)){
-		res.status(404).sendFile(path.join(__dirname,`err/404.html`));
-		return;
-	}
-
-	if(!req.path.includes(".")){
-		const rewritePath = path.join(__dirname,`${req.path}.html`);
-		if(fs.existsSync(rewritePath)){
-			res.sendFile(rewritePath);
-			return;
-		}
-	}
-	res.sendFile(path.join(__dirname,req.path));
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
 })
 
-app.use((err,req,res,next)=>{
-	console.error(err);
-	res.status(err.statusCode).sendFile(path.join(__dirname,`err/${err.statusCode}.html`));
-})
-
-app.listen(port, ()=>console.log(`Listening on ${port}`));
+app.listen(port, () => console.log(`Listening on ${port}`))
