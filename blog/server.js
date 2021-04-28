26890baacefb78819b436fe8b208d645249b387c
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-04-28 13:59:27 +0900

    Redirect blog&opendata

---
+++ blog/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const port = process.env.PORT || 3000
+const path = require('path')
+
+const app = express()
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.listen(port, () => console.log(`Listening on ${port}`))
