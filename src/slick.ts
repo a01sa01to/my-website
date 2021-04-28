274889d0f69eb1cde436750caca82b7aaf58bc19
a01sa01to <a01sa01to@users.noreply.github.com>
2021-04-28 18:32:42 +0900

    deploy: 24067deeaaf62c8b22c44703aa7580f3f91cec00

---
+++ src/slick.ts
@@ -0,0 +1,16 @@
+const $ = require('jquery');
+import "../deps/slick-1.8.1/slick/slick.js";
+
+window.addEventListener('DOMContentLoaded',()=>{
+  $('.slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    centerMode: true,
+    adaptiveHeight: true,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
+})
