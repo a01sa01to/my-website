b1d53e95322d03bcc586e652a52930e131b1dbbe
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-07-04 18:08:13 +0900

    Update server.js: wwwサブドメインの場合はwwwなしにリダイレクト

--- default/server.js
+++ default/server.js
@@ -7,7 +7,13 @@ const app = express()
 const notAllowed = require('./server_notAllowed.json')
 
 app.use((req, res) => {
+  if (req.hostname.startsWith('www')) {
+    res.redirect(301, `https://a01sa01to.com${req.path}`)
+    return
+  }
+
   console.log(req.path)
+
   for (let nonAllow of notAllowed) {
     if (req.path.match(new RegExp(nonAllow))) {
       res.status(404).sendFile(path.join(__dirname, `err/404.html`))
